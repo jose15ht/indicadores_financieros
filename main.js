e8cec8c574681dacbd0f0a3d156237dcdab0c456
José Herrera <joseignacioht@gmail.com>
2021-05-17 20:01:46 -0400

    Definido el mensaje para el programa.En esta revisión entregará los valores de UF, Dolar, Euro, IPC, Libra de Cobre y Bitcoin.Para esto, la información guardada en la variable data, se re-guardara parseada o convertida a Json en la variable information. Luego se interpolaran en el mensaje accediendo al objeto. La fecha puede ser mejorable pero por el momento es una buena opción escrita de esa forma.

--- main.js
+++ main.js
@@ -8,7 +8,15 @@ https.get('https://mindicador.cl/api', res => {
     })
 
     res.on('end', () => {
-        console.log(JSON.parse(data))
+        let information = JSON.parse(data)
+        console.log(`Gracias por consultar el servicio financiero.
+        Los indicadores para el día de hoy ${new Date(Date.now()).toLocaleDateString()} son:
+        \tUF: ${information.uf.valor}
+        \tDolar: ${information.dolar.valor}
+        \tEuro: ${information.euro.valor}
+        \tIPC: ${information.ipc.valor}
+        \tLibra de Cobre: ${information.libra_cobre.valor}
+        \tBitcoin: ${information.bitcoin.valor}`)
     })
 }).on('error', (err) => {
     console.log("Error: " + err.message)

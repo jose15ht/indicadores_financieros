e2687000d91b5a0497eb8ed917652b2bcb40691b
José Herrera <joseignacioht@gmail.com>
2021-08-22 15:26:40 -0400

    Reescritura async/await del programa

--- main.js
+++ main.js
@@ -1,23 +1,43 @@
-const https = require('https')
+const fetch = require('node-fetch')
 
-https.get('https://mindicador.cl/api', res => {
-    let data = ''
+// https.get('https://mindicador.cl/api', res => {
+//     let data = ''
 
-    res.on('data', (chunk) => {
-        data += chunk
-    })
+//     res.on('data', (chunk) => {
+//         data += chunk
+//     })
 
-    res.on('end', () => {
-        let information = JSON.parse(data)
-        console.log(`Gracias por consultar el servicio financiero.
-        Los indicadores para el día de hoy ${new Date(Date.now()).toLocaleDateString()} son:
-        \tUF: ${information.uf.valor}
-        \tDolar: ${information.dolar.valor}
-        \tEuro: ${information.euro.valor}
-        \tIPC: ${information.ipc.valor}
-        \tLibra de Cobre: ${information.libra_cobre.valor}
-        \tBitcoin: ${information.bitcoin.valor}`)
-    })
-}).on('error', (err) => {
-    console.log("Error: " + err.message)
-})
+//     res.on('end', () => {
+//         let information = JSON.parse(data)
+//         console.log(`Gracias por consultar el servicio financiero.
+//         Los indicadores para el día de hoy ${new Date(Date.now()).toLocaleDateString()} son:
+//         \tUF: ${information.uf.valor}
+//         \tDolar: ${information.dolar.valor}
+//         \tEuro: ${information.euro.valor}
+//         \tIPC: ${information.ipc.valor}
+//         \tLibra de Cobre: ${information.libra_cobre.valor}
+//         \tBitcoin: ${information.bitcoin.valor}`)
+//     })
+// }).on('error', (err) => {
+//     console.log("Error: " + err.message)
+// })
+
+const getExchange = async () => {
+    const resp = await fetch('https://mindicador.cl/api')
+    const data = await resp.json()
+    return data
+}
+
+const message = async () => {
+    const info = await getExchange()
+    console.log(`Gracias por consultar el servicio financiero.
+    Los indicadores para el día de hoy ${new Date(Date.now()).toLocaleDateString()} son:
+    \tUF: ${info.uf.valor}
+    \tDolar: ${info.dolar.valor}
+    \tEuro: ${info.euro.valor}
+   \tIPC: ${info.ipc.valor}
+    \tLibra de Cobre: ${info.libra_cobre.valor}
+   \tBitcoin: ${info.bitcoin.valor}`)
+}
+
+message()

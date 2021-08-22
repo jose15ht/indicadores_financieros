5dfabbd7fbf354bedb1fae1b3aeb26abf6ece901
José Herrera <joseignacioht@gmail.com>
2021-08-22 15:28:16 -0400

    hotfix: commit anterior no habia guardado eliminacion de lineas

--- main.js
+++ main.js
@@ -1,27 +1,5 @@
 const fetch = require('node-fetch')
 
-// https.get('https://mindicador.cl/api', res => {
-//     let data = ''
-
-//     res.on('data', (chunk) => {
-//         data += chunk
-//     })
-
-//     res.on('end', () => {
-//         let information = JSON.parse(data)
-//         console.log(`Gracias por consultar el servicio financiero.
-//         Los indicadores para el día de hoy ${new Date(Date.now()).toLocaleDateString()} son:
-//         \tUF: ${information.uf.valor}
-//         \tDolar: ${information.dolar.valor}
-//         \tEuro: ${information.euro.valor}
-//         \tIPC: ${information.ipc.valor}
-//         \tLibra de Cobre: ${information.libra_cobre.valor}
-//         \tBitcoin: ${information.bitcoin.valor}`)
-//     })
-// }).on('error', (err) => {
-//     console.log("Error: " + err.message)
-// })
-
 const getExchange = async () => {
     const resp = await fetch('https://mindicador.cl/api')
     const data = await resp.json()
